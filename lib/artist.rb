d438747a4fd90497820f10c100888b083e9847b6
Aziz Nasirov <nasirovaziz@yahoo.com>
2020-03-15 23:02:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,36 +1,29 @@
 class Artist
+  extend Concerns::Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+
   attr_accessor :name, :songs
 
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
-    save
   end
-  
+
   def self.all
     @@all
   end
-  
-  def self.destroy_all
-    self.all.clear
-  end
-  
-  def save
-    @@all << self
-  end
-  
+
   def add_song(song)
-    song.artist = self unless song.artist
-    songs.push song unless songs.include?(song)
+    @songs << song unless songs.include?(song)
+    if song.artist == nil
+      song.artist = self
+    end
   end
-  
+
   def genres
-    songs.map(&:genre).uniq
-  end
-  
-  def self.create(artist)
-    self.new(artist)
+    self.songs.collect{|song| song.genre}.uniq
   end
 end
